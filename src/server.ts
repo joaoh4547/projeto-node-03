cb360546a4565fd70d0b450167841d6f40715da1
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-24 19:49:59 -0300

    Capitulo 1 Parte 5

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import { app } from "./app";
 import { env } from "./env";
 
 app.listen({
-  host: "0.0.0.0",
-  port: env.PORT,
+    host: "0.0.0.0",
+    port: env.PORT,
 }).then(() => {
-  console.log(`Server running on port ${env.PORT} 🚀`)
-})
+    console.log(`Server running on port ${env.PORT} 🚀`);
+});
