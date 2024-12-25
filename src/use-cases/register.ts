fa0d8ef51a391ee1d576e512dc6002a0078a7c3f
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-25 18:30:34 -0300

    Capitulo 4 parte 2

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,18 +1,24 @@
 import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
 
 interface RegisterUseCaseParams {
-  name: string,
-  email: string,
-  password: string
+    name: string,
+    email: string,
+    password: string
 }
+
+interface RegisterUseCaseResult {
+    user: User
+}
+
 
 export class RegisterUseCase {
     constructor(private usersRepository: UsersRepository) {
     }
 
-    async handle({ name, email, password }: RegisterUseCaseParams) {
+    async handle({ name, email, password }: RegisterUseCaseParams): Promise<RegisterUseCaseResult> {
         const password_hash = await hash(password, 6);
 
         const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -20,8 +26,8 @@ export class RegisterUseCase {
         if (userWithSameEmail) {
             throw new UserAlreadyExistsError();
         }
-        await this.usersRepository.create({ name, email, password_hash });
-
+        const user = await this.usersRepository.create({ name, email, password_hash });
+        return { user };
     }
 }
 
--- src/use-cases/regiter.spec.ts
+++ src/use-cases/regiter.spec.ts
@@ -1,5 +1,29 @@
-import { expect, test } from "vitest";
+import { compare } from "bcryptjs";
+import { describe, expect, it } from "vitest";
+import { RegisterUseCase } from "./register";
 
-test("check if its works", () =>{
-    expect(2+2).toBe(4);
+describe("Register Use Case", () => {
+    it("should hash user password upon restriction", async () => {
+        const registerUseCase = new RegisterUseCase({
+            async create(data) {
+                return {
+                    id: "1",
+                    name: data.name,
+                    email: data.email,
+                    password_hash: data.password_hash,
+                    created_at: new Date()
+                }; 
+            },
+
+            async findByEmail(email) {
+                return null;
+            },
+
+        });
+
+        const { user } = await registerUseCase.handle({ name: "John Due", email: "jonh.due@example.com", password: "12345" });
+
+        const isPasswordCorrectlyHashed = await compare("12345", user.password_hash);
+        expect(isPasswordCorrectlyHashed).toBe(true);
+    });
 });
