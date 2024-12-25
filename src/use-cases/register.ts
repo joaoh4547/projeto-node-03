c3f001a7b629170378df86cd787c7925f4db1a1a
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-25 17:23:30 -0300

    Capitulo 3 parte  7

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import { UsersRepository } from "@/repositories/users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseParams {
@@ -8,17 +8,13 @@ interface RegisterUseCaseParams {
 }
 
 export class RegisterUseCase {
-    constructor(private usersRepository: any){
+    constructor(private usersRepository: UsersRepository) {
     }
 
     async handle({ name, email, password }: RegisterUseCaseParams) {
         const password_hash = await hash(password, 6);
 
-        const userWithSameEmail = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
+        const userWithSameEmail = await this.usersRepository.findByEmail(email);
 
         if (userWithSameEmail) {
             throw new Error("E-mail already exists.");
