48334c40dc94dcbb909be0cf24d7b081b5aa842c
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-25 15:42:54 -0300

    Capitulo 3 parte 1

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,27 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { z } from "zod";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
 
-prisma.user.create({
-    data:{
-        name: "João da Silva",
-        email: "joaosilva@example.com"
-    }
+
+app.post("/users", async (req, reply) => {
+    const registerBodySchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6)
+    });
+
+    const { email, name, password } = registerBodySchema.parse(req.body);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    });
+
+    return reply.status(201).send();
 });
