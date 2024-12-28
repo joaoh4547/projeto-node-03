85115ae4c4c6f99bdd2f01909ef4f0e542858daa
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-28 20:07:27 -0300

    Capitulo 6 parte 6

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,8 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function profile(req: FastifyRequest, reply: FastifyReply) {
-    await req.jwtVerify(); 
-    console.log(req.user.sub);
-    return reply.status(200).send();
+    const getUserProfile = makeGetUserProfileUseCase();
+    const {user} = await getUserProfile.handle({userId: req.user.sub});
+    return reply.status(200).send({user: {...user,password_hash: undefined}});
 }
