d056fae301dc492aebaea80dea91665668820911
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-28 17:41:09 -0300

    Capitulo 6 Parte 3

--- src/use-cases/factories/make-authenticate-use-case.ts
+++ src/use-cases/factories/make-authenticate-use-case.ts
@@ -3,6 +3,5 @@ import { AuthenticateUseCase } from "../authenticate";
 
 export function makeAuthenticateUseCase(){
     const usersRepository = new PrismaUsersRepository();
-    const authenticateUseCase = new AuthenticateUseCase(usersRepository);
-    return authenticateUseCase;
+    return new AuthenticateUseCase(usersRepository);
 }
--- src/use-cases/factories/make-register-use-case.ts
+++ src/use-cases/factories/make-register-use-case.ts
@@ -3,6 +3,5 @@ import { RegisterUseCase } from "../register";
 
 export function makeRegisterUseCase(){
     const usersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(usersRepository);
-    return registerUseCase;
+    return new RegisterUseCase(usersRepository);
 }
