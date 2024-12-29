f7cad5142a29e55bc73ffc22fd1ad0e0a92179ab
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-28 22:38:09 -0300

    Capitulo 6 parte 11

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,4 +1,35 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import supertest from "supertest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
+describe("Authenticate (E2E)", () => {
 
-test("ok", () =>{});
+    beforeAll(async () => {
+        await app.ready();
+    });
+    
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it("should be able to authenticate", async () => {
+        await supertest(app.server)
+            .post("/users")
+            .send({
+                name: "John Due",
+                email: "john.doe@example.com",
+                password: "123456"
+            });
+        const response = await supertest(app.server)
+            .post("/sessions")
+            .send({
+                email: "john.doe@example.com",
+                password: "123456"
+            });
+
+        expect(response.statusCode).equal(200);
+        expect(response.body).toEqual({
+            token: expect.any(String)
+        });
+    });
+});
