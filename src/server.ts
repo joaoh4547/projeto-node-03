a52bd4624a6d5452b015f32db3e7e5b543dc72ff
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2024-12-29 15:54:56 -0300

    Formatação

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ app.listen({
     host: "0.0.0.0",
     port: env.PORT,
 }).then(() => {
-    console.log(`Server running on port ${env.PORT} 🚀`); 
+    console.log(`Server running on port ${env.PORT} 🚀`);
 });
